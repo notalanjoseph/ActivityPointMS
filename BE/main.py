8397966a4fcab5b8d91ec0b27e11d5aa3c694545
Alan Joseph <notalanjoseph@gmail.com>
2023-11-16 23:26:18 +0530

    Initial commit

---
+++ BE/main.py
@@ -0,0 +1,152 @@
+from typing import Optional
+from fastapi import FastAPI, Body, HTTPException, Response, status, Depends, Form
+from fastapi.middleware.cors import CORSMiddleware
+
+import psycopg2
+from pydantic import BaseModel
+from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+
+#uvicorn BE.main:app --reload
+
+app = FastAPI()
+
+origins = ["*"] # domains which can talk to api
+
+app.add_middleware( 
+    CORSMiddleware, #middleware runs before every request
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool=True
+
+class User(BaseModel):
+    email: str
+    password: str
+        
+
+# db connection
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost', database='activities', user='postgres', password='root', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("DB connection successful!")
+        break
+    except Exception as e:
+        print("DB connection failed:\n ", e)
+        time.sleep(10)
+
+@app.get("/")
+def root():
+    return {"message":"hello wor"}
+
+
+@app.post('/login')
+#def login(user_credentials: User):
+def login(username: str = Form(...), password: str = Form(...)): #form input with names username and password are stored in vars with same name
+
+#    cursor.execute("""SELECT * FROM users WHERE email = %s""", (user_credentials.email,)) #comma to fix internal server error
+    cursor.execute("""SELECT * FROM users WHERE email = %s""", (username,)) #comma to fix internal server error
+    user = cursor.fetchone()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
+        
+    #create a token
+    #return  token
+
+    return {"token_type": "bearer"}
+
+@app.post('/users')
+#def login(user_credentials: User):
+def login(username: str = Form(...), password: str = Form(...)): #form input with names username and password are stored in vars with same name
+
+    cursor.execute("""INSERT INTO users (email, password) VALUES (%s, %s) RETURNING *""", (username, password))
+    new_user = cursor.fetchone()
+    conn.commit()
+    return {"data":new_user}
+
+@app.post("/uploads", status_code=status.HTTP_201_CREATED)
+def submit_activity(post: Post):
+    cursor.execute("""INSERT INTO actable (semester, category, title) VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data":new_post}
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.post("/createpost")
+def create_post(payload: dict = Body(...)): #extracts all fields from the body, convert to dict and store in variable payload
+    print(payload)
+    return {"message":"success"}
+
+my_posts = [{"title":"title 1", "content":"content 1", "id":1},{"title":"title 2", "content":"content 2", "id":2}] 
+
+@app.get("/posts")
+def get_posts():
+    cursor.execute("""SELECT * FROM posts """)
+    posts = cursor.fetchall()
+    return {"data": posts} 
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data":new_post}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
+    post = cursor.fetchone()
+    if not post: #if post == null
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="post with that id not found")
+    return {"post": post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT) 
+def delete_post(id: int):
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+
+    if not deleted_post:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="post with that id not found")
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+
+    if not updated_post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="post with that id not found")
+
+    return {"data": updated_post}
+
+# url/docs or url/redoc for viewing auto created documentation
+
+# now we will use db.
+# dirctory structure will be used with parent folder 'app'
---
+++ FE/route.js
@@ -0,0 +1,108 @@
+$('.message a').click(function(){
+   removeAlerts();
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+
+
+//from another pen
+
+function removeAlerts(){
+  document.getElementById("error").innerHTML = "";
+  document.getElementById("error").innerHTML = "";
+  document.getElementById("error").innerHTML = "";
+  document.getElementById("error").innerHTML = "";
+}
+
+
+
+
+function regForm() {
+  var username = document.forms["newForm"]["username"].value;
+  var pwd = document.forms["newForm"]["password"].value;
+  
+  // Create a FormData object and append the credentials
+  const formData = new FormData();
+  formData.append("username", username);
+  formData.append("password", pwd);
+
+  // Make a fetch request to your FastAPI backend
+  fetch("http://localhost:8000/users", {
+    method: "POST",
+    body: formData
+  })
+    .then((response) => {
+      if (!response.ok) {
+        document.getElementById("error").innerHTML = "ERROR";
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      document.getElementById("error").innerHTML = "success";
+      window.location.href = "index.html";
+      return response.json();
+    })
+    .then(console.log);
+}
+
+
+function validateForm() {
+  //removeAlerts();
+
+    var username = document.forms["myForm"]["username"].value;
+    var pwd = document.forms["myForm"]["password"].value;
+/*
+    if(!username && !pwd){
+        document.getElementById("error").innerHTML = "Please input username and password";
+        return false;
+    } else if(username == null || username == ""){
+		document.getElementById("error").innerHTML = "Please input your username";
+		return false;
+    } else if(pwd == null || pwd == ""){
+		document.getElementById("error").innerHTML = "Please input your password";
+		return false;
+	} else if(pwd.length < 6){
+		document.getElementById("error").innerHTML = "your password should be minimum 6 character";
+		return false;
+	}
+*/    
+    // Create a FormData object and append the credentials
+    const formData = new FormData();
+    formData.append('username', username);
+    formData.append('password', pwd);
+
+    // Make a fetch request to your FastAPI backend
+    fetch('http://localhost:8000/login', {
+      method: 'POST',
+      body: formData
+    })
+
+    .then(response => {
+      if (!response.ok) {
+        document.getElementById("error").innerHTML = "ERROR";
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      document.getElementById("error").innerHTML = "success";
+      window.location.href = "landing.html";
+      return response.json();
+    })
+
+
+            /*
+            .then(data => {
+                // Display the response on the page
+                document.getElementById('error').innerHTML = JSON.stringify(data, null, 2);
+            })
+            .catch(error => {
+                console.error('Fetch error:', error);
+            })
+            */
+    .then(console.log)  
+
+}
+
+
+
+$(document).on('click', '.form [name="logi"]', validateForm);
+$(document).on('click', '.form [name="crea"]', regForm);
+
+//$('.form button').click(validateForm());
